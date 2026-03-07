dd31978df9ab8575c9ba5826097aca2f759a28a1
josehenn7 <aldr4x.joseaugusto@gmail.com>
2026-03-07 14:40:23 -0300

    senha criptografada

---
+++ src/main/java/br/com/josehenn/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
@@ -0,0 +1,51 @@
+package br.com.josehenn.gestao_vagas.modules.company.useCases;
+
+import br.com.josehenn.gestao_vagas.modules.company.dto.AuthCompanyDTO;
+import br.com.josehenn.gestao_vagas.modules.company.repositories.CompanyRepository;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.naming.AuthenticationException;
+
+@Service
+public class AuthCompanyUseCase {
+
+    @Value("${security.token.secret}") // aplication.properties
+    private String secretKey;
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public String execute(AuthCompanyDTO authCompanyDTO) throws AuthenticationException {
+        var company = this.companyRepository.findByUsername(authCompanyDTO.getUsername()).orElseThrow(
+                () -> {
+                    throw new UsernameNotFoundException("Company not found");
+                }
+        );
+
+        // verifica se as senhas sao iguais
+        boolean passwordMatches = this.passwordEncoder.matches(authCompanyDTO.getPassword(), company.getPassword());
+
+        // se nao for igual -> erro
+        if (!passwordMatches) {
+            throw new AuthenticationException();
+        }
+
+        // se for igual -> gera token
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+        String token = JWT.create()
+                .withIssuer("javagas")
+                .withSubject(company.getId().toString())
+                .sign(algorithm);
+
+        return token;
+    }
+}
--- src/main/java/br/com/josehenn/gestao_vagas/security/SecurityConfig.java
+++ src/main/java/br/com/josehenn/gestao_vagas/security/SecurityConfig.java
@@ -4,6 +4,8 @@ package br.com.josehenn.gestao_vagas.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,10 +15,18 @@ public class SecurityConfig {
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> {
-                    auth.requestMatchers("/candidate/").permitAll().requestMatchers("/company/").permitAll();
+                    auth
+                            .requestMatchers("/candidate/").permitAll()
+                            .requestMatchers("/auth/company").permitAll()
+                            .requestMatchers("/company/").permitAll();
                     auth.anyRequest().authenticated();
                 });
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
